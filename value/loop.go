f706745e3f17c14b5e76474285da822dce971ac9
Rob Pike <r@golang.org>
2017-02-05 08:26:48 -0800

    all: apply @raduberinde's CL to dev branch - better loop termination conditions

--- value/loop.go
+++ value/loop.go
@@ -14,11 +14,9 @@ type loop struct {
 	name          string     // The name of the function we are evaluating.
 	i             uint64     // Loop count.
 	maxIterations uint64     // When to give up.
-	stallCount    int        // Iterations since |delta| changed.
 	arg           *big.Float // original argument to function; only used for diagnostic.
 	prevZ         *big.Float // Result from the previous iteration.
 	delta         *big.Float // |Change| from previous iteration.
-	prevDelta     *big.Float // Delta from the previous iteration.
 }
 
 // newLoop returns a new loop checker. The arguments are the name
@@ -33,7 +31,6 @@ func newLoop(conf *config.Config, name string, x *big.Float, itersPerBit uint) *
 		maxIterations: 10 + uint64(itersPerBit*conf.FloatPrec()),
 		prevZ:         newF(conf),
 		delta:         newF(conf),
-		prevDelta:     newF(conf),
 	}
 }
 
@@ -41,36 +38,25 @@ func newLoop(conf *config.Config, name string, x *big.Float, itersPerBit uint) *
 // after the maximum number of iterations, it errors out.
 func (l *loop) done(z *big.Float) bool {
 	l.delta.Sub(l.prevZ, z)
-	if l.delta.Sign() == 0 {
+	sign := l.delta.Sign()
+	if sign == 0 {
 		return true
 	}
-	if l.delta.Sign() < 0 {
-		// Convergence can oscillate when the calculation is nearly
-		// done and we're running out of bits. This stops that.
-		// See next comment.
+	if sign < 0 {
 		l.delta.Neg(l.delta)
 	}
-	if l.delta.Cmp(l.prevDelta) == 0 {
-		// In freaky cases (like e**3) we can hit the same large positive
-		// and then  large negative value (4.5, -4.5) so we count a few times
-		// to see that it really has stalled. Avoids having to do hard math,
-		// but it means we may iterate a few extra times. Usually, though,
-		// iteration is stopped by the zero check above, so this is fine.
-		l.stallCount++
-		if l.stallCount > 3 {
-			// Convergence has stopped.
-			return true
-		}
-	} else {
-		l.stallCount = 0
+	// Check if delta is no bigger than the smallest change in z that can be
+	// represented with the given precision.
+	var eps big.Float
+	eps.SetMantExp(eps.SetUint64(1), z.MantExp(nil)-int(z.Prec()))
+	if l.delta.Cmp(&eps) <= 0 {
+		return true
 	}
 	l.i++
 	if l.i == l.maxIterations {
 		// Users should never see this.
 		Errorf("%s %s: did not converge after %d iterations; prev,last result %s,%s delta %s", l.name, l.arg, l.maxIterations, BigFloat{z}, BigFloat{l.prevZ}, BigFloat{l.delta})
 	}
-	l.prevDelta.Set(l.delta)
 	l.prevZ.Set(z)
 	return false
-
 }
