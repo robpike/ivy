5ff993c0dc3b55953217431c5230391a3f9c3550
Rob Pike <r@golang.org>
2020-06-25 20:48:27 +1000

    doc: run go generate
    Forgot in the last commit

--- mobile/help.go
+++ mobile/help.go
@@ -305,8 +305,8 @@ is not specified. For these commands, numbers are always read and printed
 base 10 and must be non-negative on input.
 </p>
 <pre>) help
-	List the special commands. )help &lt;topic&gt; to learn more about
-	a topic.
+	Describe the special commands. Run )help &lt;topic&gt; to learn more
+	about a topic, )help &lt;op&gt; to learn more about an operator.
 ) base 0
 	Set the number base for input and output. The commands ibase and
 	obase control setting of the base for input and output alone,
