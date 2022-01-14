2721d24a37f4a8254bf273eb00b2bd021af592dd
Rob Pike <r@golang.org>
2022-01-14 20:17:14 +1100

    ivy: fix bug in evaluation of **-1
    The Taylor series for exp(x) begins 1 + x, so exp(-1) hits zero.
    Fix this - perhaps not the right fix, but easy - but requiring
    loops to run at least 3 iterations.
    
    Fixes #96.

--- value/loop.go
+++ value/loop.go
@@ -36,10 +36,13 @@ func newLoop(conf *config.Config, name string, x *big.Float, itersPerBit uint) *
 
 // done reports whether the loop is done. If it does not converge
 // after the maximum number of iterations, it errors out.
+// It will not return before doing at least 3 iterations. Some
+// series (such as exp(-1) hit zero along the way.
 func (l *loop) done(z *big.Float) bool {
+	const minIterations = 3
 	l.delta.Sub(l.prevZ, z)
 	sign := l.delta.Sign()
-	if sign == 0 {
+	if sign == 0 && l.i >= minIterations {
 		return true
 	}
 	if sign < 0 {
@@ -49,7 +52,7 @@ func (l *loop) done(z *big.Float) bool {
 	// represented with the given precision.
 	var eps big.Float
 	eps.SetMantExp(eps.SetUint64(1), z.MantExp(nil)-int(z.Prec()))
-	if l.delta.Cmp(&eps) <= 0 {
+	if l.delta.Cmp(&eps) <= 0 && l.i >= minIterations {
 		return true
 	}
 	l.i++
