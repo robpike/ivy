c642447f3e01abf3bb74ee1966ce794765a64f85
Rob Pike <r@golang.org>
2025-01-26 07:34:02 +1100

    ivy: fix typo in comment: s/flatternTo/flattenTo/
    Fixes #218

--- value/eval.go
+++ value/eval.go
@@ -1036,7 +1036,7 @@ func flatten(v Value) iter.Seq2[int, Value] {
 	}
 }
 
-// flatternTo flattens the values contained in v,
+// flattenTo flattens the values contained in v,
 // caling yield(off, x0), yield(off+1, x1), ... for successive values.
 // It returns the new next offset to use
 // and whether the iteration should continue at all.
