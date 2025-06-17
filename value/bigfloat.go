a8c511a44e14daff51ff1935d00753250bc7c843
Denis Bernard <db047h@gmail.com>
2025-06-17 04:51:56 +0200

    value: fix fastFloatPrint mode in bigFloat.Sprint (#229)
    - BigFloat.Sprint: perform the argument reduction with extended
      precision and reorganize the code to fallback to big.Float.Text()
      after the rescale check
    - constants:
      - reduce precision of integer and rational constants (using more
        precision than needed to represent the values is not necessary
        and adversely impacts performance)
      - float constants are now allocated with 32 bits of extra precision
      - update hard-coded values of pi, e, ln 2 and ln 10 to have at least
        10000 + 32 bits of precision
    - refactor exponential to match the big.Float API
    - refactor newFxP -> newFP to take a single prec argument
    
    Fixes #225

--- value/bigfloat.go
+++ value/bigfloat.go
@@ -36,9 +36,6 @@ func (f BigFloat) String() string {
 func (f BigFloat) Sprint(conf *config.Config) string {
 	var mant big.Float
 	exp := f.Float.MantExp(&mant)
-	// MantExp sets mant.prec = f.float.prec. Force mant.prec to conf.FloatPrec()
-	// in order to avoid unexpected rounding issues.
-	mant.SetPrec(conf.FloatPrec())
 	positive := 1
 	if exp < 0 {
 		positive = 0
@@ -59,7 +56,6 @@ func (f BigFloat) Sprint(conf *config.Config) string {
 	// first. It is however less feature-complete.
 	// (Big ints are problematic too, but if you print 1e50000000
 	// as an integer you probably won't be surprised it's slow.)
-
 	if fastFloatPrint && exp > 10000 {
 		// We always use %g to print the fraction, and it will
 		// never have an exponent, but if the format is %E we
@@ -68,7 +64,10 @@ func (f BigFloat) Sprint(conf *config.Config) string {
 		if verb == 'E' || verb == 'G' {
 			eChar = 'E'
 		}
-		fexp := newF(conf).SetInt64(int64(exp))
+		// Up to precision 10000, the result is off by 4 decimal digits.
+		// Add at least 4×ln(10)/ln(2) bits of precision.
+		fprec := addPrec(conf.FloatPrec(), 16)
+		fexp := newFP(fprec).SetInt64(int64(exp))
 		fexp.Mul(fexp, floatLog2)
 		fexp.Quo(fexp, floatLog10)
 		// We now have a floating-point base 10 exponent.
@@ -76,49 +75,38 @@ func (f BigFloat) Sprint(conf *config.Config) string {
 		// The integer part is what we will show.
 		// The 10**(fractional part) will be multiplied back in.
 		iexp, _ := fexp.Int(nil)
-		fraction := fexp.Sub(fexp, newF(conf).SetInt(iexp))
+		fraction := fexp.Sub(fexp, newFP(fprec).SetInt(iexp))
 		// Now compute 10**(fractional part).
 		// Fraction is in base 10. Move it to base e.
 		fraction.Mul(fraction, floatLog10)
-		scale := exponential(conf, fraction)
+		scale := exponential(&big.Float{}, fraction)
+		sign := ""
+		if mant.Sign() < 0 {
+			sign = "-"
+			mant.Neg(&mant)
+		}
+		mant.SetPrec(fprec)
 		if positive > 0 {
 			mant.Mul(&mant, scale)
 		} else {
 			mant.Quo(&mant, scale)
 		}
-		ten := newF(conf).SetInt64(10)
 		i64exp := iexp.Int64()
-		// For numbers not too far from one, print without the E notation.
-		// Shouldn't happen (exp must be large to get here) but just
-		// in case, we keep this around.
-		if -4 <= i64exp && i64exp <= 11 {
-			if i64exp > 0 {
-				for i := 0; i < int(i64exp); i++ {
-					mant.Mul(&mant, ten)
-				}
+		// If it has a leading 0 rescale.
+		digits := mant.Text('g', prec)
+		if digits[0] == '0' {
+			mant.Mul(&mant, new(big.Float).SetUint64(10))
+			if positive > 0 {
+				i64exp--
 			} else {
-				for i := 0; i < int(-i64exp); i++ {
-					mant.Quo(&mant, ten)
-				}
+				i64exp++
 			}
-			return fmt.Sprintf("%g\n", &mant)
-		} else {
-			sign := ""
-			if mant.Sign() < 0 {
-				sign = "-"
-				mant.Neg(&mant)
-			}
-			// If it has a leading zero, rescale.
-			digits := mant.Text('g', prec)
-			for digits[0] == '0' {
-				mant.Mul(&mant, ten)
-				if positive > 0 {
-					i64exp--
-				} else {
-					i64exp++
-				}
-				digits = mant.Text('g', prec)
-			}
+			digits = mant.Text('g', prec)
+		}
+		// Print with the E notation for numbers far enough from one.
+		// This should always be the case (exp must be large to get here) but just
+		// in case, we keep this check around and fallback to big.Float.Text.
+		if i64exp < -4 || 11 < i64exp {
 			return fmt.Sprintf("%s%s%c%c%d", sign, digits, eChar, "-+"[positive], i64exp)
 		}
 	}
