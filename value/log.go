ccfba273bff11e906ccaad4a133955236e0b5260
Rob Pike <r@golang.org>
2025-11-07 16:18:14 +1100

    ivy: fix integer logs
    The code was just wrong. Rewrite it for clarity.
    Avoid the squaring for now, although it might be worth
    revisiting. The cost is noticeable:
    
    2 log 2**1e5
    100000
    
    )cpu
    2.686ms (1.818ms user, 655µs sys)
    
    Fixes #234

--- value/log.go
+++ value/log.go
@@ -31,14 +31,14 @@ func logBaseU(c Context, u, v Value) Value {
 	var i Int
 	switch u := u.(type) {
 	case Int:
-		i, v = intLog(u, v.(Int))
-		if v.(Int) == 1 {
-			return i
+		log, exact := intLog(u, v.(Int))
+		if exact {
+			return Int(log)
 		}
 	case BigInt:
-		i, v = bigIntLog(u, v.(BigInt))
-		if v.(BigInt).Cmp(bigIntOne.Int) == 0 {
-			return i
+		log, exact := bigIntLog(u, v.(BigInt))
+		if exact {
+			return log
 		}
 	}
 	f := c.EvalBinary(logn(c, v), "/", logn(c, u))
@@ -48,60 +48,48 @@ func logBaseU(c Context, u, v Value) Value {
 	return f
 }
 
-// intLog returns the integer portion i of log base b of v
-// along with the remaining portion v / b^i.
-func intLog(b, v Int) (i, r Int) {
-	if b <= 1 || v < 0 {
-		return 0, v
+// intLog returns the integer portion of the log base b of v, if it's exact.
+func intLog(bI, vI Int) (log uint64, exact bool) {
+	b := uint64(bI)
+	v := uint64(vI)
+	if b <= 1 || v < b {
+		return 0, false
 	}
-	iu, ru := uint64Log(uint64(b), uint64(v))
-	return Int(iu), Int(ru)
+	log = 1 // The logarithm is at least 1 because v>=b.
+	// Looping one division at a time is easy but slow.
+	// Doesn't matter much here but can be important in bigIntLog.
+	for v != b {
+		quo := v / b
+		rem := v % b
+		if rem != 0 {
+			return 0, false
+		}
+		log++
+		v = quo
+	}
+	return log, true
 }
 
-// uint64Log is like intLog, but for uint64.
-// Working in uint64 makes the overflow check easy.
-func uint64Log(b, v uint64) (i, r uint64) {
-	// Log by repeated squaring produces a single bit at a time, high to low.
-	// The algorithm is the reverse of exponentiation by repeated squaring,
-	if b <= 1 || v < b || v%b != 0 {
-		return 0, v
-	}
-	// u doesn't fit in 32 bits, then b*b will both overflow the uint64
-	// and be larger than v (if not for the overflow),
-	// so only recurse when b does fit in 32 bits
-	if uint64(uint32(b)) == b {
-		i, v = uint64Log(b*b, v)
-		i <<= 1
-	}
-	if v >= b {
-		v /= b
-		i |= 1
-	}
-	return i, v
-}
-
-// bigIntLog is like intLog, but for BigInt.
-// Note that the integer log portion always fits in an Int:
-// any non-trivial result is using base b ≥ 2,
-// bounding the result by the number of bits in v.
-func bigIntLog(b, v BigInt) (i Int, r BigInt) {
+// bigIntLog returns the integer portion of the log base b of v, if it's exact.
+func bigIntLog(b, v BigInt) (log Int, exact bool) {
 	if b.Cmp(bigIntOne.Int) <= 0 || v.Cmp(b.Int) < 0 {
-		return 0, v
+		return 0, false
 	}
-	if z := new(big.Int).Mod(v.Int, b.Int); z.Cmp(bigIntZero.Int) != 0 {
-		return 0, v
+	log = Int(1) // The logarithm is at least 1 because v>=b.
+	x := new(big.Int).Set(v.Int)
+	quo := new(big.Int)
+	rem := new(big.Int)
+	// Looping one division at a time is easy but slow.
+	// TODO: Scale up faster.
+	for x.Cmp(b.Int) != 0 {
+		quo.DivMod(x, b.Int, rem)
+		if rem.Sign() != 0 {
+			return 0, false
+		}
+		log++
+		x.Set(quo)
 	}
-
-	// Only compute b*b if it can be smaller than v.
-	if 2*b.BitLen()-1 <= v.BitLen() {
-		i, v = bigIntLog(BigInt{new(big.Int).Mul(b.Int, b.Int)}, v)
-		i <<= 1
-	}
-	if v.Cmp(b.Int) >= 0 {
-		v = BigInt{new(big.Int).Div(v.Int, b.Int)}
-		i |= 1
-	}
-	return i, v
+	return log, true
 }
 
 // floatLog computes natural log(x) using the Maclaurin series for log(1-x).
